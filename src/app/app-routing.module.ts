e36b155c58a825079513130bf7b8099378e9dae7
REDA Laghrissi <93975710+LAGHRISSIREDA@users.noreply.github.com>
2023-01-24 16:51:27 +0100

    add admin component , admin route and add some admin instruction to dashboard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from "@angular/core";
 import { RouterModule , Routes } from "@angular/router";
+import { AdminComponent } from "./admin/admin/admin.component";
 import { LoginComponent } from "./auth/login/login.component";
 import { RegisterComponent } from "./auth/register/register.component";
 import { HomeComponent } from "./home/home.component";
 
 const routes : Routes = [
     {path : '' ,   component : HomeComponent},
-    // {path : 'cart' ,   component : CartComponent},
+    {path : 'admin' ,   component : AdminComponent},
     {path : 'login' ,   component : LoginComponent},
     {path : 'register' ,   component : RegisterComponent},
 
